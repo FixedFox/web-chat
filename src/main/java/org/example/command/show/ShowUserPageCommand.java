9052e89ae6c80ed6ef91d73a11d05b4a5db56f22
fixedfox <fixedfox@live.ru>
2024-02-19 21:12:00 +0300

    Исправил возможность открытия страницы пользователей только для роли Admin

--- src/main/java/org/example/command/show/ShowUserPageCommand.java
+++ src/main/java/org/example/command/show/ShowUserPageCommand.java
@@ -6,13 +6,19 @@ import javax.servlet.http.HttpServletResponse;
 import org.example.command.Command;
 import org.example.result.ForwardResult;
 import org.example.result.Result;
+
+import static org.example.Resources.PAGE_CHAT;
 import static org.example.Resources.PAGE_USERS;
 import static org.example.data.DataBase.showAllUsers;
 
 public class ShowUserPageCommand implements Command {
     @Override
     public Result execute(HttpServletRequest request, HttpServletResponse response) {
-        request.getSession().setAttribute("users", showAllUsers());
-        return new ForwardResult(PAGE_USERS);
+        if ((boolean) request.getSession().getAttribute("isAdmin")) {
+            request.getSession().setAttribute("users", showAllUsers());
+            return new ForwardResult(PAGE_USERS);
+        } else {
+            return new ForwardResult(PAGE_CHAT);
+        }
     }
 }
